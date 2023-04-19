9087ae52e6c3c40e472fd1d0a8cc29826ac90d1d
lesca fang <lesca.fang@bcs-ais.com>
2023-04-19 15:00:37 +0800

    update cors proxy

--- 敲键背词/anki_phonetic_dev.js
+++ 敲键背词/anki_phonetic_dev.js
@@ -43,34 +43,3 @@ async function getPhoneticSymbols(word) {
 
   return phoneticSymbols
 }
-
-// v2
-function getPhoneticSymbols(word) {
-  cors_proxy = "http://u.silkdiy.com:3066"
-  fetch(`${cors_proxy}/https://dict.youdao.com/jsonapi_s?q=${word}&le=en&t=2&client=web&sign=30ac8a1fffc5c066d343a8eb2ef8572e&keyfrom=webdict`)
-  .then ((response) => {return response.json()})
-  .catch(error => console.error('Error:', error))
-  .then(response => {
-    // get
-    phoneticSymbols = {
-      "ukphone": response.simple.word[0].ukphone,
-      "usphone": response.simple.word[0].usphone
-    };
-    // console.log(phoneticSymbols)
-    // generate html
-    phonetic_html = ""
-    if (phoneticSymbols.ukphone) {
-      phonetic_html += `英 /${phoneticSymbols.ukphone}/` 
-    }
-    if (phoneticSymbols.usphone) {
-      if (phonetic_html) {
-        phonetic_html += `<br>美 /${phoneticSymbols.usphone}/` 
-      } else {
-        phonetic_html += `美 /${phoneticSymbols.usphone}/` 
-      }
-    }
-    // replace
-    var phonetic_symobls = document.querySelector("#phonetic_symobls");
-    phonetic_symobls.innerHTML = phonetic_html
-  });
-}
